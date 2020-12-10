32e5cf11fb775f7c958873ee50a2d8dccad7f3ab
bechir-kamkoum <bechirkamkoum@gmail.com>
2020-12-10 18:48:14 +0100

    react

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import {Form, Button, InputGroup , FormControl} from 'react-bootstrap'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Form>
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+<div>
+  <InputGroup className="mb-3">
+    <InputGroup.Prepend>
+      <InputGroup.Checkbox aria-label="Checkbox for following text input" />
+    </InputGroup.Prepend>
+    <FormControl aria-label="Text input with checkbox" />
+  </InputGroup>
+  <InputGroup>
+    <InputGroup.Prepend>
+      <InputGroup.Radio aria-label="Radio button for following text input" />
+    </InputGroup.Prepend>
+    <FormControl aria-label="Text input with radio button" />
+  </InputGroup>
+</div>
+      </div>
+  );
+}
+
+export default App;
