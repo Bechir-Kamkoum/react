54194350d91d5c12b9335347c4a4f97b58cdca60
bechir-kamkoum <bechirkamkoum@gmail.com>
2020-12-15 12:59:28 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,44 +1,18 @@
-import {Form, Button, InputGroup , FormControl} from 'react-bootstrap'
 import './App.css';
-
 function App() {
   return (
     <div className="App">
-     <Form>
-  <Form.Group controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
-    <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </Form.Text>
-  </Form.Group>
+      <h1 className="title">Your name here</h1>
+      <br />
+      <img src="paysagepublic.jpg" alt="paysagePublic"/>
+      <br />
+      <img src={"./src/paysageSource.jpg"} alt ='paysageSource'/>
+      <br />
+      <video width="320" height="240" controls>
 
-  <Form.Group controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
-  </Form.Group>
-  <Form.Group controlId="formBasicCheckbox">
-    <Form.Check type="checkbox" label="Check me out" />
-  </Form.Group>
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
-</Form>
-<div>
-  <InputGroup className="mb-3">
-    <InputGroup.Prepend>
-      <InputGroup.Checkbox aria-label="Checkbox for following text input" />
-    </InputGroup.Prepend>
-    <FormControl aria-label="Text input with checkbox" />
-  </InputGroup>
-  <InputGroup>
-    <InputGroup.Prepend>
-      <InputGroup.Radio aria-label="Radio button for following text input" />
-    </InputGroup.Prepend>
-    <FormControl aria-label="Text input with radio button" />
-  </InputGroup>
-</div>
-      </div>
+      <source src="nature-short-video.mp4" type="video/mp4"></source></video>
+
+    </div>
   );
 }
 
